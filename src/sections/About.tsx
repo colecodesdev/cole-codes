9ca1ade7b81445265e9073d3bd5e83e31cbaec31
colto <creill14@my.wgu.edu>
2026-03-06 20:26:35 -0600

    adjust map

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -78,7 +78,7 @@ export const AboutSection = () => {
               <Image
                 src={newMapImage}
                 alt="Map showing my location"
-                className="absolute inset-0 h-full w-full object-cover object-left-top"
+                className="absolute inset-0 h-full w-full object-cover object-[left_30%]"
               />
               <div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 size-20 rounded-full after:content-[""] after:absolute after:inset-0 after:outline after:outline-2 after:-outline-offset-2 after:rounded-full after:outline-gray-950/30'>
                 <div className="absolute inset-0 rounded-full bg-gradient-to-r from-emerald-300 to-sky-400 -z-20 animate-ping [animation-duration:2s]"></div>
